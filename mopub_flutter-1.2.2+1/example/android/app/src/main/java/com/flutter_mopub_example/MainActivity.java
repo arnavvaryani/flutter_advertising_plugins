6140f5f8a446984598e7e4e83b81cb1be952a11c
Dewey Gaedcke <dewey@pathoz.com>
2021-10-15 14:30:35 -0500

    start of config for V2 embedding

--- mopub_flutter-1.2.2+1/example/android/app/src/main/java/com/flutter_mopub_example/MainActivity.java
+++ mopub_flutter-1.2.2+1/example/android/app/src/main/java/com/flutter_mopub_example/MainActivity.java
@@ -1,13 +1,14 @@
 package com.mopub_flutter_example;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+// import android.os.Bundle;
+// import io.flutter.app.FlutterActivity;
+// import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+  // @Override
+  // protected void onCreate(Bundle savedInstanceState) {
+  //   super.onCreate(savedInstanceState);
+  //   GeneratedPluginRegistrant.registerWith(this);
+  // }
 }
