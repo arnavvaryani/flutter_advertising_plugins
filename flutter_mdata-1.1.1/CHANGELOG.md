04106b3491a4f974f99f58da4c04c2dc21f13b7a
Arnav Varyani <37153449+arnavvaryani@users.noreply.github.com>
2023-10-28 15:39:41 -0400

    Add files via upload

---
+++ flutter_mdata-1.1.1/CHANGELOG.md
@@ -0,0 +1,4 @@
+## 1.1.0+1
+Null safety, dependency update
+## 1.1.1
+dependency update
---
+++ flutter_mdata-1.1.1/example/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mdata/flutter_mdata.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  void isSDKReady(bool isReady) {
+    if (isReady) {
+      MoneDataSDK.instance.consent();
+    }
+  }
+
+  void consentListener(bool consent) {
+    print('consent: $consent');
+    MoneDataSDK.instance.start();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('MoneDataSDK example app'),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text("Init SDK"),
+              onPressed: () {
+                MoneDataSDK.instance.init(
+                    apiToken: 'api_token',
+                    oguryToken: 'ogury_token');
+                MoneDataSDK.instance.setConsentListener(consentListener);
+                MoneDataSDK.instance.setisReadyListener(isSDKReady);
+              },
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
